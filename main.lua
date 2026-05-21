eaac044cf911f59b6a1a05084de5175d39212455
ozymandiaslone <saltsperkins@gmail.com>
2026-05-21 18:56:35 +0000

    fifth

--- main.lua
+++ main.lua
@@ -1,35 +1,30 @@
-local HIRED_HAND_TYPE = ENT_TYPE.MONS_HIREDHAND
+local HIRED_HAND_TYPE = ENT_TYPE.CHAR_HIREDHAND
 
--- We use a flag to load textures once, only after the game is ready
-local textures_loaded = false
-local tex_biden = -1
-local tex_trump = -1
+local tex_biden
+local tex_trump
 
 set_callback(function()
-    if not textures_loaded and HIRED_HAND_TYPE ~= nil then
-        local base_type = get_type(HIRED_HAND_TYPE)
-        
-        -- Biden Setup
-        local biden_def = get_texture_definition(base_type.texture)
-        biden_def.texture_path = "sprites/hired_help_biden.png"
-        tex_biden = define_texture(biden_def)
-        
-        -- Trump Setup
-        local trump_def = get_texture_definition(base_type.texture)
-        trump_def.texture_path = "sprites/hired_help_trump.png"
-        tex_trump = define_texture(trump_def)
-        
-        textures_loaded = true
-    end
-end, ON.GAME_START)
 
-set_callback(function(entity)
-    if textures_loaded then
-        local choice = prng:random(1, 2)
-        if choice == 1 then
-            entity:set_texture(tex_biden)
-        else
-            entity:set_texture(tex_trump)
-        end
+    local base = get_type(HIRED_HAND_TYPE)
+
+    -- Biden
+    local biden_def = get_texture_definition(base.texture)
+    biden_def.texture_path = "sprites/hired_help_biden.png"
+    tex_biden = define_texture(biden_def)
+
+    -- Trump
+    local trump_def = get_texture_definition(base.texture)
+    trump_def.texture_path = "sprites/hired_help_trump.png"
+    tex_trump = define_texture(trump_def)
+
+end, ON.START)
+
+set_post_entity_spawn(function(ent)
+
+    if math.random(1,2) == 1 then
+        ent:set_texture(tex_biden)
+    else
+        ent:set_texture(tex_trump)
     end
-end, ON.ENTITY_INITIALIZE, HIRED_HAND_TYPE)
+
+end, SPAWN_TYPE.ANY, MASK.PLAYER, HIRED_HAND_TYPE)
