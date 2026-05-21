6d4fbf3ef6df18dd542d91f1ff334b267b01e502
ozymandiaslone <saltsperkins@gmail.com>
2026-05-21 18:44:50 +0000

    third

--- main.lua
+++ main.lua
@@ -1,3 +1,30 @@
+local HIRED_HELP_TYPE = ENT_TYPE.MONS_HIREDHELP
+
+-- 1. Grab the default texture ID from the Entity Database
+local default_texture_id = get_type(HIRED_HELP_TYPE).texture
+
+-- 2. Define the Biden texture template
+local biden_def = get_texture_definition(default_texture_id)
+biden_def.texture_path = "sprites/hired_help_biden.png"
+local texture_biden = define_texture(biden_def)
+
+-- 3. Define the Trump texture template
+local trump_def = get_texture_definition(default_texture_id)
+trump_def.texture_path = "sprites/hired_help_trump.png"
+local texture_trump = define_texture(trump_def)
+
+-- 4. Roll the dice when a Hired Help spawns
+set_callback(function(entity)
+    -- Generate a random number: 1 or 2
+    local choice = prng:random(1, 2)
+    
+    if choice == 1 then
+        entity:set_texture(texture_biden)
+    else
+        entity:set_texture(texture_trump)
+    end
+end, ON.ENTITY_INITIALIZE, HIRED_HELP_TYPE)
+
 local init_done = false
 local texture_biden = -1
 local texture_trump = -1
